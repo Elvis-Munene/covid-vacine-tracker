c236ff7a446889bc4b3b78173b76719c1a51b444
Elvis-Munene <elvismuneneg@gmail.com>
2023-01-12 20:23:45 -0800

    renders Vaccine data on Vaccine component

--- src/components/dashboard/Vaccine.jsx
+++ src/components/dashboard/Vaccine.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import VaccineCard from './VaccineCard'
-import './Vaccine.css'
+import React from "react";
+import VaccineCard from "./VaccineCard";
+import "./Vaccine.css";
 
-function Vaccine({vaccineStats}) {
-console.log(vaccineStats)
+function Vaccine({ vaccineStats }) {
+  console.log(vaccineStats);
+
+ 
+
   return (
     <>
-    <div className="vaccine_class">
-     <div className="vaccine_title" id='#vaccine'><b>Vaccines</b></div>
-     <div className="vaccine_component">
-     <div className='vaccine_component_1'><VaccineCard title={"Here goes the title"} content="Here goes the content"/></div>
-     <div className='vaccine_component_1'><VaccineCard title={"Here goes the title"} content="Here goes the content"/></div>
-     <div className='vaccine_component_1'><VaccineCard title={"Here goes the title"} content="Here goes the content"/></div>
-     <div className='vaccine_component_1'><VaccineCard title={"Here goes the title"} content="Here goes the content"/></div>
-     <div className='vaccine_component_1'><VaccineCard title={"Here goes the title"} content="Here goes the content"/></div>
-     </div>
-     </div>
-     </>
-  )
+      <div className="vaccine_class">
+        <div className="vaccine_title" id="#vaccine">
+          <b>Vaccines</b>
+        </div>
+        <div className="vaccine_component">
+          {vaccineStats.map((vaccine, index) => {
+            return (
+              <VaccineCard
+                key={index}
+                title={vaccine.trimedName}
+                content={vaccine.category}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Vaccine
+export default Vaccine;
