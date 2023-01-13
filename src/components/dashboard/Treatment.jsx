3eeab7d230b4600689dc565b9d0478bd7c335d5f
Elvis-Munene <elvismuneneg@gmail.com>
2023-01-13 00:41:33 -0800

    styles the treatment component

--- src/components/dashboard/Treatment.jsx
+++ src/components/dashboard/Treatment.jsx
@@ -7,7 +7,7 @@ function Treatment({treatmentStats}) {
   return (
     <>
     <div className='treatment_class'>
-    <div id='treatment'><b>Treatment</b></div>
+    <div id='treatment'><b>The following are some of the covid 19 treatments that have been done</b></div>
    
          { treatmentStats?.map((treatment, index) => {
     return(
--- src/components/dashboard/TreatmentCard.jsx
+++ src/components/dashboard/TreatmentCard.jsx
@@ -5,13 +5,23 @@ function TreatmentCard( {title, content}) {
   return (
   <>
    <div className='treatment'>
-        <div className='treatment_title'>
+        {/* <div className='treatment_title'>
             {title}
         </div>
         <div className='treatment_content'>
            <p>{content}</p>
 
-        </div>
+        </div> */}
+
+<div className="card  mb-3">
+    <div className="card">
+      <div className="card-body">
+        Treatment Name:<h5 className="card-title">{title}</h5>
+        Treatment Category:<p className="card-text"><h6>{content}</h6></p>
+      
+      </div>
+    </div>
+  </div>
 
     </div>
   </>
